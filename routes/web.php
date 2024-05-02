b166d514f561c4ea7f9710212746642dade41cf7
sahyudi <sahyudi.dev@gmail.com>
2024-05-02 18:47:49 +0700

    add template sb admin2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Home;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/', [Home::class, 'index']);
